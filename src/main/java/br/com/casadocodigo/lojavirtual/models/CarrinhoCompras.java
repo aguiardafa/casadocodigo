3a578a55419b297e9e508304a569c8d05e501f19
aguiardafa <diego.fernandes.aguiar@gmail.com>
2018-07-26 01:59:46 -0300

    definindo o SCOPO de alguns elementos da aplicação

--- src/main/java/br/com/casadocodigo/lojavirtual/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/lojavirtual/models/CarrinhoCompras.java
@@ -3,9 +3,14 @@ package br.com.casadocodigo.lojavirtual.models;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
+// quando faz-se necessário que um recurso seja individual, ou seja, unico por usuário
+// definimos o recurso como escopo de sessão
 @Component
+@Scope(value=WebApplicationContext.SCOPE_SESSION) 
 public class CarrinhoCompras {
 
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
