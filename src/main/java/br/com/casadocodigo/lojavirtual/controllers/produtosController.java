d598a60a3508810cfae5c848d15f4468b99eb615
aguiardafa <diego.fernandes.aguiar@gmail.com>
2018-08-07 17:02:57 -0300

    implementação de integração de servicos com JSON através do jackson

--- src/main/java/br/com/casadocodigo/lojavirtual/controllers/produtosController.java
+++ src/main/java/br/com/casadocodigo/lojavirtual/controllers/produtosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -95,4 +96,12 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
+	@RequestMapping("/{id}")
+	@ResponseBody // informar ao Spring que o retorno será no corpo da resposta da requisição
+	public Produto detalheJSON(@PathVariable("id") Integer id){
+	    // É importante notar que só funcionou, 
+		// porque o Jackson está configurado no projeto
+		return produtoDAO.find(id);
+	}
+	
 }
